60642692eae5d54be755bda568890ebb659c23fe
kungfumas <eri299@gmail.com>
2018-03-30 19:44:23 +0700

    edisi revisi
    ini adalah perbaikan dari versi sebelumnya

--- jsonmhsw.php
+++ jsonmhsw.php
@@ -2,19 +2,14 @@
 // Check for the path elements
 // Turn off error reporting
 error_reporting(0);
-
 // Report runtime errors
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
-
 // Report all errors
 error_reporting(E_ALL);
-
 // Same as error_reporting(E_ALL);
 ini_set("error_reporting", E_ALL);
-
 // Report all errors except E_NOTICE
 error_reporting(E_ALL & ~E_NOTICE);
-
 $path = $_SERVER[PATH_INFO];    
 if ($path != null) {
 $path_params = spliti ("/", $path);
@@ -27,14 +22,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	{
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
-
 //
 $input = file_get_contents("php://input");
-$json = json_decode($string, true);
-foreach ($json_a as $key => $value){
+
+$json = json_decode($input, true);
+echo "json =".$json["nim"] ;
+echo "Proses".$json->nim;
+$nimmhsw=$json["nim"];
+$namamhsw=$json["nama"];
+$prodimhsw=$json["prodi"];
+/*foreach ($json as $key => $value){
   echo  $key . ':' . $value;
-}
-		$querycek = "SELECT nim,nama,prodi FROM mahasiswa WHERE nim ='$xml->nim'";
+}*/
+		$querycek = "SELECT nim,nama,prodi FROM mahasiswa WHERE nim ='$nimmhsw'";
 		echo "query select ".$querycek;
 	//if ($result=mysqli_query($con,querycek))
 	//{
@@ -54,23 +54,22 @@ foreach ($json_a as $key => $value){
 		nama,
 		prodi)
 		VALUES (				
-		'$xml->nim',
-		'$xml->nama',
-		'$xml->progdi')";
+		'$nimmhsw',
+		'$namamhsw',
+		'$prodimhsw')";
 		echo "query ".$query;
 		mysqli_query($con,$query);
 		}
 		else if ($rowcount > 0)
 		{
 		$query = "UPDATE mahasiswa SET
-		nama = '$xml->nama',
-		prodi = '$xml->progdi'
-		WHERE nim ='$xml->nim'";
+		nama = '$namamhsw',
+		prodi = '$prodimhsw'
+		WHERE nim ='$nimmhsw'";
 		echo "query ".$query;
 		mysqli_query($con,$query);
 		}
 	//}
 mysqli_close($con);
 }
-
 ?> 
